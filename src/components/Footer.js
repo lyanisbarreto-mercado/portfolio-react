f9a1c5d41a2ddd418c91dfc12319e9bbc27136bf
Lyanis Barreto <lyanis.barreto@gmail.com>
2026-06-04 17:18:16 -0400

    Updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,19 +3,21 @@ import React from 'react'
 const Footer = () => {
   return (
     <div id="footer">
-      <div>
-        <h4>Resources</h4>
-        <ul>
-            <li>React</li>
-            <li>, CSS</li>
-            <li>, VS Code</li>
-            <li>, Bootstrap</li>
-            <li>, Framer Motion</li>
-        </ul>
-        </div>
-        <a href="#">Back to top</a>
-        <div>
-          <p>All rights reserved @2026</p>
+      <div id="footer-content">
+        <div >
+          <h4>Resources</h4>
+          <ul id="site-resourses">
+              <li>React</li>
+              <li>, CSS</li>
+              <li>, VS Code</li>
+              <li>, Bootstrap</li>
+              <li>, Framer Motion</li>
+          </ul>
+          </div>
+          <a href="#">Back to top</a>
+          <div>
+            <p>All rights reserved @2026</p>
+          </div>
         </div>
     </div>
   )
